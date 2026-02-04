c834c5c11d2aa98083541cae40f522106a096d36
Tommaso De Nova <denovatommaso@gmail.com>
2026-02-04 08:45:57 -0600

    commit

--- lib/maps/osm.ts
+++ lib/maps/osm.ts
@@ -1,10 +1,10 @@
 export const OSM_STYLE_URL =
   process.env.EXPO_PUBLIC_OSM_STYLE_URL ||
-  'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json';
+  'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
 
 export const OSM_TILE_URL =
   process.env.EXPO_PUBLIC_OSM_TILE_URL ||
-  'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png';
+  'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png';
 
 export const OSM_ATTRIBUTION =
   process.env.EXPO_PUBLIC_OSM_ATTRIBUTION || '© OpenStreetMap contributors © CARTO';
